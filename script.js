ab39d887a51845c03e80931a7d2f5ba1a718b3ea
Ilya <ilya.saenko.06@mail.ru>
2025-02-20 12:20:57 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const telegramInput = document.getElementById('telegram');
+    const submitButton = document.getElementById('submit-button');
+console.log(submitButton);
+    // Устанавливаем начальное значение для поля Telegram
+    telegramInput.value = 'https://t.me/';
+
+    // Обработчик для кнопки "Связаться со мной"
+    submitButton.addEventListener('click', function() {
+        // Обновляем страницу
+        location.reload();
+    });
+
+    // Обработчик для поля Telegram, чтобы пользователь мог ввести только ID
+    telegramInput.addEventListener('input', function() {
+        const baseUrl = 'https://t.me/';
+        if (!telegramInput.value.startsWith(baseUrl)) {
+            telegramInput.value = baseUrl;
+        }
+    });
+
+    // Обработчик для поля Telegram, чтобы запретить удаление базовой части ссылки
+    telegramInput.addEventListener('keydown', function(event) {
+        const baseUrl = 'https://t.me/';
+        if (event.key === 'Backspace' && telegramInput.value.length <= baseUrl.length) {
+            event.preventDefault();
+        }
+    });
+});
